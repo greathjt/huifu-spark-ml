e48b5c35a7fb45d716185802e816c5c5373662e9
alex <greathjt@163.com>
2016-12-02 19:10:31 +0800

    init

--- src/main/java/com/huifu/spark/ml/Word2VecApp.java
+++ src/main/java/com/huifu/spark/ml/Word2VecApp.java
@@ -5,10 +5,16 @@ import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.spark.JavaHBaseContext;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.ml.feature.Word2Vec;
 import org.apache.spark.ml.feature.Word2VecModel;
 import org.apache.spark.mllib.linalg.Vector;
@@ -22,26 +28,29 @@ import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
+import scala.Tuple2;
+
 public class Word2VecApp {
   public static void main(String[] args) {
-    //create spark context
+    // create spark context
     SparkConf scf = new SparkConf().setMaster("local").setAppName("Word2VecApp");
     JavaSparkContext jsc = new JavaSparkContext(scf);
     SQLContext sqlContext = new SQLContext(jsc);
     // Connect to Hbase table BD_PAGE_REPOSITORY
     Configuration conf = HBaseConfiguration.create();
-     JavaHBaseContext hbaseContext = new JavaHBaseContext(jsc, conf);
-//    
-//    Configuration conf = HBaseConfiguration.create();
-//    // 设置查询条件，这里值返回用户的等级
-//    Scan scan = new Scan();
-//    scan.addColumn(Bytes.toBytes("URL"),Bytes.toBytes("filteredPlainText"));
-    
+    JavaHBaseContext hbaseContext = new JavaHBaseContext(jsc, conf);
+    Scan scan = new Scan();
+    scan.addColumn(Bytes.toBytes("URL"), Bytes.toBytes("filteredPlainText"));
+    scan.setCaching(100);
+    scan.setMaxResultSize(2000);
+    JavaRDD<Tuple2<ImmutableBytesWritable, Result>> javaRdd =
+        hbaseContext.hbaseRDD(TableName.valueOf("BD_PAGE_REPOSITORY"), scan);
+    JavaRDD<Row> jrdd = javaRdd.map(new ScanConvertFunction());
     // Input data: Each row is a bag of words from a sentence or document.
-    JavaRDD<Row> jrdd = jsc.parallelize(
-        Arrays.asList(RowFactory.create(Arrays.asList("Hi I heard about Spark".split(" "))),
-            RowFactory.create(Arrays.asList("I wish Java could use case classes".split(" "))),
-            RowFactory.create(Arrays.asList("Logistic regression models are neat".split(" ")))));
+    // JavaRDD<Row> jrdd = jsc.parallelize(
+    // Arrays.asList(RowFactory.create(Arrays.asList("Hi I heard about Spark".split(" "))),
+    // RowFactory.create(Arrays.asList("I wish Java could use case classes".split(" "))),
+    // RowFactory.create(Arrays.asList("Logistic regression models are neat".split(" ")))));
     //
     StructType schema = new StructType(new StructField[] {new StructField("text",
         new ArrayType(DataTypes.StringType, true), false, Metadata.empty())});
@@ -58,8 +67,16 @@ public class Word2VecApp {
       Vector vector = (Vector) row.get(1);
       System.out.println("Text: " + text + " => \nVector: " + vector + "\n");
     }
-    DataFrame syn = model.findSynonyms("about", 3);
-    System.out.println(syn.collectAsList().get(0).getDouble(1));
+  }
+
+  private static class ScanConvertFunction
+      implements Function<Tuple2<ImmutableBytesWritable, Result>, Row> {
+    @Override
+    public Row call(Tuple2<ImmutableBytesWritable, Result> arg0) throws Exception {
+      return null;
+    }
+
+
   }
 
 }
