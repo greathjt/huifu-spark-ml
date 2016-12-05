ef00fa2a67d677a64824fd429fa954758f4f5d60
alex <greathjt@163.com>
2016-12-05 16:34:18 +0800

    modify local word2vec

--- src/main/java/com/huifu/spark/ml/Word2VecApp.java
+++ src/main/java/com/huifu/spark/ml/Word2VecApp.java
@@ -1,5 +1,6 @@
 package com.huifu.spark.ml;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,7 +19,6 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.ml.feature.Word2Vec;
 import org.apache.spark.ml.feature.Word2VecModel;
-import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
@@ -61,13 +61,19 @@ public class Word2VecApp {
         hbaseContext.hbaseRDD(TableName.valueOf("BD_PAGE_REPOSITORY"), scan);
     JavaRDD<Row> jrdd = javaRdd.map(new ScanConvertFunction());
     //
+    // List<Row> jrdd =
+    // Arrays.asList(RowFactory.create(Arrays.asList("Hi I heard about Spark".split(" "))),
+    // RowFactory.create(Arrays.asList("I wish Java could use case classes".split(" "))),
+    // RowFactory.create(Arrays.asList("Logistic regression models are neat".split(" "))));
+
+
     StructType schema = new StructType(new StructField[] {new StructField("text",
         new ArrayType(DataTypes.StringType, true), false, Metadata.empty())});
 
     DataFrame documentDF = sqlContext.createDataFrame(jrdd, schema);
     // Learn a mapping from words to Vectors.
     Word2Vec word2Vec = new Word2Vec().setInputCol("text").setOutputCol("result").setVectorSize(400)
-        .setMinCount(5).setWindowSize(5);
+        .setMinCount(0).setWindowSize(5);
 
     Word2VecModel model = word2Vec.fit(documentDF);
     // DataFrame result = model.transform(documentDF);
@@ -79,12 +85,15 @@ public class Word2VecApp {
     String[] keyWords =
         "无耻,诈骗,骗子,垃圾,跑路,失联,维权,欺骗,造假,无法取现,不让取现,提现困难,坑爹,黑名单,曝光,自融,作死,坑,忽悠,警惕,疑似,小心,泄露,恶意,撤销,忠告,不能提现,危险,提现,谎称,信息披露,嫌疑,虚假,涉嫌,卑鄙,血汗钱,预警,奇葩,澄清,亏空,待收,证据,失信,忽悠,逾期,人去楼空,虚假标的,揭发,电话打不通,不兑现,圈套,破案,案件,还我钱,被抓,风险,取保候审,小心,非法,集资,警察,立案"
             .split(",");
+    // String[] keyWords = "Logistic,regression,models,are,neat".split(",");
     for (String keyWord : keyWords) {
-      DataFrame result = model.findSynonyms(keyWord, 25);
-      for (Row row : result.collectAsList()) {
-        List<String> text = row.getList(0);
-        Vector vector = (Vector) row.get(1);
-        System.out.println("Text: " + text + " => \nVector: " + vector + "\n");
+      try {
+        List<Row> result = model.findSynonyms(keyWord, 25).collectAsList();
+        // System.out.println("Text: " + keyWord result.get(0).getString(0) );
+        // System.out.println("Text: " + result );
+        System.out.println("keyword:" + keyWord + ",synonyms:" + result.toString());
+      } catch (Exception e) {
+        e.printStackTrace();
       }
     }
   }
