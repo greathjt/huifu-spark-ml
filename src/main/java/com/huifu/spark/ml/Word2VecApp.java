fb4efb7d8b4cdf45e1920378e245a685fb2e5975
alex <greathjt@163.com>
2016-12-09 10:22:17 +0800

    for test

--- src/main/java/com/huifu/spark/ml/Word2VecApp.java
+++ src/main/java/com/huifu/spark/ml/Word2VecApp.java
@@ -41,8 +41,7 @@ public class Word2VecApp {
 
   public static void main(String[] args) throws IOException {
     // init context
-    // System.setProperty("hadoop.home.dir", "c:\\\\winutil\\\\");
-    scf = new SparkConf().setMaster("local").setAppName("Word2VecApp");
+    scf = new SparkConf().setAppName("Word2VecApp");
     jsc = new JavaSparkContext(scf);
     sqlContext = new SQLContext(jsc);
     // Connect to Hbase table BD_PAGE_REPOSITORY
@@ -58,17 +57,12 @@ public class Word2VecApp {
     // create and save model
     Word2VecModel model = createWord2VecModel();
     // Find similar word
-    findWordSynonyms("造假", model);
-    // keywords' synonyms
-    // String[] keyWords =
-    // "无耻,诈骗,骗子,垃圾,跑路,失联,维权,欺骗,造假,无法取现,不让取现,提现困难,坑爹,黑名单,曝光,自融,作死,坑,忽悠,警惕,疑似,小心,泄露,恶意,撤销,忠告,不能提现,危险,提现,谎称,信息披露,嫌疑,虚假,涉嫌,卑鄙,血汗钱,预警,奇葩,澄清,亏空,待收,证据,失信,忽悠,逾期,人去楼空,虚假标的,揭发,电话打不通,不兑现,圈套,破案,案件,还我钱,被抓,风险,取保候审,小心,非法,集资,警察,立案"
-    // .split(",");
-    // String[] keyWords = "Logistic,regression,models,are,neat".split(",");
-    /**
-     * for (String keyWord : keyWords) { try { List<Row> result = model.findSynonyms(keyWord,
-     * 25).collectAsList(); System.out.println("keyword:" + keyWord + ",synonyms:" +
-     * result.toString()); } catch (Exception e) { e.printStackTrace(); } }
-     **/
+    String[] keyWords =
+        "无耻,诈骗,骗子,垃圾,跑路,失联,维权,欺骗,造假,无法取现,不让取现,提现困难,坑爹,黑名单,曝光,自融,作死,坑,忽悠,警惕,疑似,小心,泄露,恶意,撤销,忠告,不能提现,危险,提现,谎称,信息披露,嫌疑,虚假,涉嫌,卑鄙,血汗钱,预警,奇葩,澄清,亏空,待收,证据,失信,忽悠,逾期,人去楼空,虚假标的,揭发,电话打不通,不兑现,圈套,破案,案件,还我钱,被抓,风险,取保候审,小心,非法,集资,警察,立案"
+            .split(",");
+    for (String keyWord : keyWords) {
+      findWordSynonyms(keyWord, model);
+    }
   }
 
   private static void findWordSynonyms(String str, Word2VecModel model) {
@@ -83,28 +77,25 @@ public class Word2VecApp {
     JavaHBaseContext hbaseContext = new JavaHBaseContext(jsc, conf);
     Scan scan = new Scan();
     scan.addColumn(Bytes.toBytes("URL"), Bytes.toBytes("filteredPlainText"));
-    scan.setCaching(100);
     scan.setMaxVersions(1);
+    scan.setBatch(5000);
+    scan.setStartRow(Bytes.toBytes("00001-cn.babymoney.news-95DDA939AA334F2EE4C2161696D889D3"));
+    scan.setStopRow(Bytes.toBytes("00001-cn.com.sina.nmg-94CFBC836560B05458CC34BE85762C30"));
     // scan.setFilter(new PageFilter(2000));
     JavaRDD<Tuple2<ImmutableBytesWritable, Result>> javaRdd =
         hbaseContext.hbaseRDD(TableName.valueOf("BD_PAGE_REPOSITORY"), scan);
     JavaRDD<Row> jrdd = javaRdd.map(new ScanConvertFunction());
-    //
-    // List<Row> jrdd =
-    // Arrays.asList(RowFactory.create(Arrays.asList("Hi I heard about Spark".split(" "))),
-    // RowFactory.create(Arrays.asList("I wish Java could use case classes".split(" "))),
-    // RowFactory.create(Arrays.asList("Logistic regression models are neat".split(" "))));
-
 
     StructType schema = new StructType(new StructField[] {new StructField("text",
         new ArrayType(DataTypes.StringType, true), false, Metadata.empty())});
 
     DataFrame documentDF = sqlContext.createDataFrame(jrdd, schema);
     // Learn a mapping from words to Vectors.
-    Word2Vec word2Vec = new Word2Vec().setInputCol("text").setOutputCol("result").setVectorSize(400)
-        .setMinCount(0).setWindowSize(5);
+    Word2Vec word2Vec =
+        new Word2Vec().setInputCol("text").setOutputCol("result").setVectorSize(400);
 
     Word2VecModel model = word2Vec.fit(documentDF);
+    
     // DataFrame result = model.transform(documentDF);
     // for (Row row : result.collectAsList()) {
     // List<String> text = row.getList(0);
@@ -117,6 +108,7 @@ public class Word2VecApp {
     // } else {
     //
     // }
+    
     return model;
   }
 
